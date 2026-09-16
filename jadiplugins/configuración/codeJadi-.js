29cb2e797b7d441844c0d0f3506e8d53c033455f
Iᴀɴ.xʏᴢ <taiwam34@gmail.com>
2026-09-15 21:59:06 -0300

    -

--- jadiplugins/configuración/codeJadi-.js
+++ jadiplugins/configuración/codeJadi-.js
@@ -64,7 +64,7 @@ export default {
                 'https://tewianix.org'
 
             const previewImage =
-                'https://files.catbox.moe/fhnqaa.jpg'
+                'https://files.catbox.moe/rdn7sk.jpg'
 
             let linkPreview = null
 
